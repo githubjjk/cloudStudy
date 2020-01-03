cba315107fa49afa1d73e665a3304b78420231b2
gitjjk <1519328942@qq.com>
2020-01-03 11:44:37 +0800

    [1.新增] 一些闲余文件

---
+++ cs-gateway/src/test/java/jjk/csgateway/CsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package jjk.csgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
