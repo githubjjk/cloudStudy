d2237e14240076fbdd67c5e24cc40e8e858027fa
gitjjk <1519328942@qq.com>
2020-01-03 11:19:16 +0800

    [3.删除] 删除copy.bat文件

--- copy.bat
+++
@@ -1,4 +0,0 @@
-copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-auth\target\cs-auth-0.0.1-SNAPSHOT.jar D:\项目\javaProject\cloudStudyStart
-copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-utils\target\cs-utils-0.0.1-SNAPSHOT.jar D:\项目\javaProject\cloudStudyStart
-copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-server\target\cs-server-0.0.1-SNAPSHOT.jar D:\项目\javaProject\cloudStudyStart
-pause
