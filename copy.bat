05e85331f72ca313b13df302f08026e5517cc37e
gitjjk <1519328942@qq.com>
2020-02-26 17:13:09 +0800

    [2.修改] copy.bat命令

--- copy.bat
+++ copy.bat
@@ -1,4 +1,4 @@
-copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-auth\target\cs-auth-0.0.1-SNAPSHOT.jar C:\Users\57485\Desktop\cloudStudy
-copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-server\target\cs-server-0.0.1-SNAPSHOT.jar C:\Users\57485\Desktop\cloudStudy
-copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-gateway\target\cs-gateway-0.0.1-SNAPSHOT.jar C:\Users\57485\Desktop\cloudStudy
+copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-auth\target\cs-auth-1.0-SNAPSHOT.jar C:\Users\57485\Desktop\cloudStudy
+copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-server\target\cs-server-1.0-SNAPSHOT.jar C:\Users\57485\Desktop\cloudStudy
+copy C:\Users\57485\.jenkins\workspace\cloudStudy\cs-gateway\target\cs-gateway-1.0-SNAPSHOT.jar C:\Users\57485\Desktop\cloudStudy
 pause
